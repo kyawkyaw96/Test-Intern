f3df8dd2a0718a9dd44d47023c81ec49892ac9b4
Kyaw Kyaw <winkyaw.demon@gmail.com>
2024-12-17 18:31:40 +0700

    1_of_3

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Test1 from "./Test1/Test1";
+import Test2 from "./Test2/Test2";
+import './index.css'
+
+const App = () => {
+    
+  return (
+    <div >
+      
+     <Test1/>
+     <Test2/>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Test1/Test1.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import PatternGen from "./PatternGen";
+
+const Test1 = () => {
+  return (
+    <div>
+      <h1>Center</h1>
+      <PatternGen type="centered" />
+      <h1>left</h1>
+      <PatternGen type="left" />
+      <h1>custom</h1>
+      <PatternGen type="custom" />
+      <h1>Right</h1>
+      <PatternGen type="right" />
+    </div>
+  );
+};
+
+export default Test1;
